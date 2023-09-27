51c37fec68f86bd3dec01e31cbf483d9849cb166
Yash Jain <46950318+yjpictures@users.noreply.github.com>
2023-09-27 16:59:35 -0400

    added basic auth (#9)

--- server/main.py
+++ server/main.py
@@ -1,20 +1,44 @@
 from flask import Flask, redirect, url_for, request, abort, render_template
 from flask_cors import CORS
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_selfdoc import Autodoc
 from database import MongoDB
 import pandas as pd
 
+dB = MongoDB()
+dB.connect()
 app = Flask(__name__)
 CORS(app)
 auto_doc = Autodoc(app)
-dB = MongoDB()
-dB.connect()
+auth = HTTPBasicAuth()
+
+users = {
+	'admin': generate_password_hash(dB.adminPWD),
+	'manager': generate_password_hash(dB.managerPWD),
+	'client': generate_password_hash(dB.clientPWD)
+}
+
+@auth.verify_password
+def verify_password(username, password):
+	if username in users and check_password_hash(users.get(username), password): # type: ignore
+		return username
+	
+@auth.get_user_roles
+def get_user_roles(user):
+	return user
+
+@auth.error_handler
+def auth_error(status):
+	return {'message': 'You do not have access'}, status
 
 @app.route("/api/v1/create", methods=['POST'])
 @auto_doc.doc()
+@auth.login_required(role=['manager', 'admin'])
 def create():
 	"""
-	This can be used to create a new license
+	This can be used to create a new license.
+	Authorized access: 'manager', 'admin'
 	"""
 	try:
 		response = dB.create(request.get_json())
@@ -24,9 +48,11 @@ def create():
 
 @app.route("/api/v1/renew", methods=['PATCH'])
 @auto_doc.doc()
+@auth.login_required(role=['manager', 'admin'])
 def renew():
 	"""
-	This can be used to renew a license
+	This can be used to renew a license.
+	Authorized access: 'manager', 'admin'
 	"""
 	try:
 		dB.renew(request.get_json())
@@ -36,9 +62,11 @@ def renew():
 
 @app.route("/api/v1/delete", methods=['DELETE'])
 @auto_doc.doc()
+@auth.login_required(role=['manager', 'admin'])
 def delete():
 	"""
-	This can be used to delete a license
+	This can be used to delete a license.
+	Authorized access: 'manager', 'admin'
 	"""
 	try:
 		dB.delete(request.get_json())
@@ -48,9 +76,11 @@ def delete():
 
 @app.route("/api/v1/validate", methods=['GET'])
 @auto_doc.doc()
+@auth.login_required
 def validate():
 	"""
-	This can be used to validate a license
+	This can be used to validate a license.
+	Authorized access: 'client', 'manager', 'admin'
 	"""
 	try:
 		response = dB.validate(request.get_json())
@@ -59,9 +89,12 @@ def validate():
 		return {'message': str(e)}, 404
 	
 @app.route("/api/v1/get-all", methods=['GET'])
+@auto_doc.doc()
+@auth.login_required(role=['admin'])
 def getAll():
 	"""
-	This can be used to get all the licenses from database
+	This can be used to get all the licenses from database.
+	Authorized access: 'admin'
 	"""
 	try:
 		response = dB.getAll()
@@ -70,9 +103,11 @@ def getAll():
 		return {'message': str(e)}, 404
 	
 @app.route("/api/v1/database", methods=['GET'])
+@auth.login_required(role=['admin'])
 def showDatabase():
 	"""
-	This can be used to get all the licenses from database
+	This can be used to get all the licenses from database.
+	Authorized access: 'admin'
 	"""
 	try:
 		response = dB.getAll()
