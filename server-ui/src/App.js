66c7becc09ba69db8834475cceb245125b20a18a
Yash Jain <yashjain@mail.com>
2023-10-01 19:24:40 -0400

    added VSCode launch and task

--- server-ui/src/App.js
+++ server-ui/src/App.js
@@ -3,7 +3,6 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Route, Routes, BrowserRouter } from "react-router-dom";
 import TopBar from './components/TopBar';
 
 const darkTheme = createTheme({
@@ -19,15 +18,13 @@ export default function App() {
 
   return (
 	<ThemeProvider theme={darkTheme}>
-	  <BrowserRouter>
 		<Box sx={{ display: 'flex' }}>
-		  <CssBaseline />
-		  <TopBar />
-		  <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-			<Toolbar />
-		  </Box>
+			<CssBaseline />
+			<TopBar />
+			<Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+				<Toolbar />
+			</Box>
 		</Box>
-	  </BrowserRouter>
 	</ThemeProvider>
   );
 }
