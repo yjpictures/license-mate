003dd00db57f45cd92a7d23278f255641faf288a
Yash Jain <yashjain@mail.com>
2023-10-04 12:27:31 -0400

    updated indentation

--- server-ui/src/Api.js
+++ server-ui/src/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: '/api/v1/',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+	baseURL: '/api/v1/',
+	headers: {
+		'Content-Type': 'application/json'
+	}
 });
